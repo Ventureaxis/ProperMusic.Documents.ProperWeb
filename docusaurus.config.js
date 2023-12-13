15603ffd7adc08768d5b8818b1b3cb381ad050d8
Matthew Staton <matthew.staton@ventureaxis.com>
2023-12-13 09:31:29 +0000

    add config for github pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'Ventureaxis', // Usually your GitHub org/user name.
   projectName: 'propermusic.documents.properweb', // Usually your repo name.
-
+  trailingSlash: false,
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
